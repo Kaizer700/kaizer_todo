0cdd832c5075e42e13392c9ae7059b4c94b8a6c8
Jeffery237 <wankijefferson@gmail.com>
2024-06-18 12:45:58 +0100

    Teuma Sadie completed Task 2 ToDo-List

---
+++ Teuma/admin.py
@@ -0,0 +1,8 @@
+# Kaizer/admin.py
+
+from django.contrib import admin
+from .models import Profile, Task, Tag
+
+admin.site.register(Profile)
+admin.site.register(Task)
+admin.site.register(Tag)
---
+++ Teuma/forms.py
@@ -0,0 +1,9 @@
+# Kaizer/forms.py
+
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'due_date', 'status', 'tags']
